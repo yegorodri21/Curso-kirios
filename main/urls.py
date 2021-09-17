52eac007abb76afded3efc727928d667518053f4
yegorodri21 <70181300+yegorodri21@users.noreply.github.com>
2021-09-17 18:27:41 -0500

    17/09

--- main/urls.py
+++ main/urls.py
@@ -9,4 +9,5 @@ urlpatterns =[
     path ('registro/',views.registro, name="registro"),
     path ('logout/', views.logout_request, name="logout"),
     path ('login/', views.login_request, name ="login"),
+    path ('curso_form/', views.curso_form,name="curso_form"),
 ]
