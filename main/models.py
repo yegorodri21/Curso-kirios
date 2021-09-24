cb185e2e8f1f41c8c4197e36048b0c5f55b0a37f
yegorodri21 <70181300+yegorodri21@users.noreply.github.com>
2021-09-23 20:39:57 -0500

    23/09

--- main/models.py
+++ main/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from datetime import datetime
-# Create your models here.
+
 class Curso(models.Model):
     curso_titulo=models.CharField(max_length=200)
     curso_contenido=models.TextField()
     curso_publicado=models.DateTimeField(
         "fecha de publicacion", default=datetime.now())
     foto=models.ImageField('albums', upload_to='albums', null=True)
-    # foto=models.ImageField(upload_to='albums', null=True)
 
     def __str__(self):
         return self.curso_titulo
 
+class Registrom (models.Model):
+    nombre=models.TextField(max_length=200)
+    apellido=models.TextField(max_length=200)
+    cedula=models.DecimalField(max_digits=15, decimal_places=14, default=0)
+    materia=models.CharField(max_length=200)
+    fecha=models.DateTimeField(
+        "fecha de matricula", default=datetime.now())
+
+    def __str__(self):
+        return self.Cedula
